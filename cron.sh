c72403e6f0ebc8a1c2009249d66f5774e1aba862
MalachiteN <malachiten@163.com>
2023-03-22 21:43:25 +0800

    Speeded up kdsy-stat and made it quicker, but dependent on SQLite

--- cron.sh
+++ cron.sh
@@ -1,4 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
+SHELL=/data/data/com.termux/files/usr/bin/bash
+PREFIX=/data/data/com.termux/files/usr
+PATH=/data/data/com.termux/files/usr/bin:/data/data/com.termux/files/home/.local/share/kdsy
+MAILTO=root
+HOME=/data/data/com.termux/files/home
+KDSY=/data/data/com.termux/files/home/.local/share/kdsy
 
 h=`date -Ih |sed -e's/^.*T//' -e's/+.*$//'`
 if ((h < 7)) ;then
@@ -8,6 +14,6 @@ fi
 
 echo -e '\e[33m=== === ===		BEGIN		=== === ===\e[0m'
 echo `date`
-$KDSY/kdsy-gather -s 60
+kdsy-gather -s 60
 echo `date`
 echo -e '\e[33m=== === ===		END		=== === ===\e[0m\n'

a2dbba3c7a8f335e3a9d1e121e2ae08875a5050a
MalachiteN <malachiten@163.com>
2023-04-10 10:28:21 +0800

    kdsy-gather now outputs every last change time

--- cron.sh
+++ cron.sh
@@ -6,14 +6,18 @@ MAILTO=root
 HOME=/data/data/com.termux/files/home
 KDSY=/data/data/com.termux/files/home/.local/share/kdsy
 
+sz=40
+
 h=`date -Ih |sed -e's/^.*T//' -e's/+.*$//'`
 if ((h < 7)) ;then
-echo '此时没有必要收集'
-exit
+	echo '此时没有必要收集'
+	exit
+elif ((h == 7)) ;then
+	sz=300
 fi
 
 echo -e '\e[33m=== === ===		BEGIN		=== === ===\e[0m'
 echo `date`
-kdsy-gather -s 40
+kdsy-gather -s $sz
 echo `date`
 echo -e '\e[33m=== === ===		END		=== === ===\e[0m\n'
